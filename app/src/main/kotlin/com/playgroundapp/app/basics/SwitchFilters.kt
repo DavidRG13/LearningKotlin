a2f350040d641bc2d7150f20708c19668aa47b4a
david <DAVIDrg?>
2015-04-20 14:10:04 +0200

    Switch

---
+++ app/src/main/kotlin/com/playgroundapp/app/basics/SwitchFilters.kt
@@ -0,0 +1,27 @@
+package basics
+
+import java.io.File
+
+//fun main(args: Array<String>) {
+//
+//    val a: Any = File("ee")
+//    when (a){
+//        1 -> println("1")
+//        2 -> println("2")
+//        in 2..4 -> println("2..4")
+//        !in 2..4 -> println("NO 2..4")
+//        !is Number -> println("No number")
+//        else -> println("Otra cosa")
+//    }
+//
+//
+//    when (a){
+//        a == 1 -> println("1")
+//        a == 2 -> println("2")
+//        a in 2..4 -> println("2..4")
+//        a !in 2..4 -> println("NO 2..4")
+//        a !is Number -> println("No number")
+//        else -> println("Otra cosa")
+//    }
+//}
+
