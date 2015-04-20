9180e0f585bbc2f380adbe5e0c86ce76fa1568c6
david <DAVIDrg?>
2015-04-20 14:10:29 +0200

    Default and named params

---
+++ app/src/main/kotlin/com/playgroundapp/app/basics/DefaultNamedParams.kt
@@ -0,0 +1,13 @@
+package basics
+
+fun doSomething(i: Int = 1, s: String = "abc") {
+    println("$i -> $s")
+}
+
+//fun main(args: Array<String>) {
+//
+//    doSomething()
+//    doSomething(i = 3)
+//    doSomething(s = "Wolaa")
+//    doSomething(2, "eeee")
+//}
