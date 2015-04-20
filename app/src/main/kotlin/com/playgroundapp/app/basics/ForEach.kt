0c014e247361dcb7e37a99688e4b08272c2bca9f
david <DAVIDrg?>
2015-04-20 14:10:39 +0200

    Loops

---
+++ app/src/main/kotlin/com/playgroundapp/app/basics/ForEach.kt
@@ -0,0 +1,16 @@
+package basics
+
+//fun main(args: Array<String>) {
+//
+//    // List<String> list = Arrays.asList("a", "b", "c", "d")
+//    // for (String elements : list) {
+//
+//    // }
+//
+//
+//
+//    val list = listOf("a", "b", "c", "d");
+//    for (element in list) {
+//
+//    }
+//}
---
+++ app/src/main/kotlin/com/playgroundapp/app/basics/ForEachWithIndex.kt
@@ -0,0 +1,18 @@
+package basics
+
+//fun main(args: Array<String>) {
+//
+//    // List<String> list = Arrays.asList("a", "b", "c", "d")
+//    // int index = 0;
+//    // for (String elements : list) {
+//    //    System.out.println(index + " -> " + element);
+//    //    index++;
+//    // }
+//
+//
+//
+//    val list = listOf("a", "b", "c", "d");
+//    for ((index, element) in list.withIndex()) {
+//        println("$index -> $element")
+//    }
+//}
