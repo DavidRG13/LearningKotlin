42d3a013ebde332ec7c9da952aad240e7c148b64
david <DAVIDrg?>
2015-04-20 14:11:33 +0200

    Lambdas

---
+++ app/src/main/kotlin/com/playgroundapp/app/advanced/lambdas.kt
@@ -0,0 +1,21 @@
+package advanced
+
+//fun main(args: Array<String>) {
+//
+//    val users = listOf(
+//            User("", "", 31),
+//            User("", "", 29)
+//    )
+//
+//    val over30 = users.filter ({ (user: User): Boolean -> user.getAge() > 30 })
+//
+//    val over30 = users.filter { u -> u.getAge() > 30 }
+//
+//    val over30 = users.filter { it.getAge() > 30 }
+//
+//    val predicate: (User) -> Boolean = {it.getAge() > 30}
+//
+//    val over30 = users.filter(predicate)
+//
+//}
+
