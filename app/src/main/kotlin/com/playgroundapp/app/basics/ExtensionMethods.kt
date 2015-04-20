4483ad3cca737a5bad3efde0314643c22706e3ec
david <DAVIDrg?>
2015-04-20 14:11:07 +0200

    Extension methods

---
+++ app/src/main/kotlin/com/playgroundapp/app/basics/ExtensionMethods.kt
@@ -0,0 +1,28 @@
+package basics
+
+//fun String.lastChar() = this.get(this.length() - 1)
+fun String.lastChar() = get(length() - 1)
+
+//fun main(args: Array<String>) {
+//
+//    println("abc".lastChar())
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//    for(character in "abc"){
+//
+//    }
+//
+//    for((index, character) in "abc".withIndex()) {
+//
+//    }
+//}
