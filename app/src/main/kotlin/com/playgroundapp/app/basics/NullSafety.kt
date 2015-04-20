99fb7f9e2aeefb28fe0d65a1b96f5d669cbf57b7
david <DAVIDrg?>
2015-04-20 14:10:51 +0200

    Null safety

---
+++ app/src/main/kotlin/com/playgroundapp/app/basics/NullSafety.kt
@@ -0,0 +1,43 @@
+package basics
+
+import java.io.File
+
+//fun main(args: Array<String>) {
+//
+//    var text1: String = "something"
+//
+//    var text2: String = null
+//
+//    var text3: String? = null
+//
+//
+//
+//
+//    text3.replace(" ", "'")
+//    text3?.replace(" ", "'")
+//    text3!!.replace(" ", "'")
+//
+//
+//
+//
+//
+//
+//
+//    /*
+//    int count = new File("test").listFiles().lenth;
+//    System.out.println(count);
+//     */
+//
+//    val files: Array<File>? = File("test").listFiles()
+//    println(files.size())   // it can be null
+//
+//    if(files != null){
+//        println(files.size())
+//    }
+//
+//
+//    println(files?.size())   // if != null
+//    println(files?.size() ?: 0)   // if == null print 0
+//
+//
+//}
